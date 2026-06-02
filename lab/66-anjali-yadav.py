68c57094629a0b82c8b730f705c7b8486896001c
anjaliyadav32607-create <anjaliyadav32607@gmail.com>
2026-06-02 12:04:26 +0530

    Update 66-anjali-yadav.py

--- lab/66-anjali-yadav.py
+++ lab/66-anjali-yadav.py
@@ -1,1 +1,29 @@
-
+a=4
+b=9
+c=19
+def number(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+number(a)
+number(b)
+number(c)
+#problem 3
+a=4 
+b=9 
+c=19
+def number(n):
+    if n%3==0:
+        print("divisible by 3")
+    else:
+        print("not divisible")
+number(a)
+number(b)
+number(c)
+#problem 4
+a=[1,2,3]
+def print_list(list):
+ for x in list:
+     print(x)
+print_list(a)
