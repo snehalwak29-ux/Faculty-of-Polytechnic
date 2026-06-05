a0d8717f3743383b30cda26fe563c47dac343aed
ayushkamutdak-creator <ayushkamutdak@gmail.com>
2026-06-05 11:37:59 +0530

    Update Aayushka-f2.py

--- students/aayushka-mutdak/Aayushka-f2.py
+++ students/aayushka-mutdak/Aayushka-f2.py
@@ -2,7 +2,6 @@
 s={'a','b','c','d'}
 s.update({'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'})
 print(s)
-count=0
 count= 26
 vowels ={'a','e','i', 'o', 'u'}
 
@@ -10,6 +9,18 @@ for x in s:
      if x in vowels:
         count = count - 1
 
-print("Vowel count:", count)
+print("consonets count:", count)
+#problem2
+s={'a','b','c','d'}
+s.update({'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'})
+print(s)
+count= 0
+vowels ={'a','e','i', 'o', 'u'}
+
+for x in s:
+     if x in vowels:
+        count = count +1
+print ("vowels count:",count)
+
 
 
